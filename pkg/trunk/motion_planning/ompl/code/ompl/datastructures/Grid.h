e51535520aa42ad975670c7d603adf86da36a72e
WG <http://www.willowgarage.com>
2008-11-21 05:38:04 +0000

    Changes for building on OS X
    --HG--
    extra : convert_revision : 6e515b53a3f6f67f969083c68eb3fe13467ad91d

--- pkg/trunk/motion_planning/ompl/code/ompl/datastructures/Grid.h
+++ pkg/trunk/motion_planning/ompl/code/ompl/datastructures/Grid.h
@@ -40,9 +40,19 @@
 #include <vector>
 
 #ifdef __GNUC__
-#  include <features.h>
+// OS X doesn't have features.h.  Instead we'll just paste in the
+// definition of __GNUC_PREREQ() taken from a Linux version of features.h.
+//#  include <features.h>
+#ifndef __GNUC_PREREQ
+#if defined __GNUC__ && defined __GNUC_MINOR__
+# define __GNUC_PREREQ(maj, min) \
+                ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#else
+# define __GNUC_PREREQ(maj, min) 0
+#endif
+#endif
 
-#  if __GNUC_PREREQ(4,0)
+#  if __GNUC_PREREQ(4,1)
 #    include <tr1/unordered_map>
 #    define OMPL_NS_HASH std::tr1
 #    define OMPL_NAME_HASH unordered_map
