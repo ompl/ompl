92f8724666f9234c1d3c427523874395e3de5b4a
Ioan Sucan <isucan@gmail.com>
2009-03-07 05:55:00 +0000

    initial base class for kinodynamic planning
    --HG--
    extra : convert_revision : 4eca2176f3773590f882cc78a2d4411b626f40d3

---
+++ pkg/trunk/motion_planning/ompl/code/ompl/extension/samplingbased/kinodynamic/Control.h
@@ -0,0 +1,81 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+* 
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+* 
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* \author Ioan Sucan */
+
+#ifndef OMPL_EXTENSION_SAMPLINGBASED_KINODYNAMIC_CONTROL_
+#define OMPL_EXTENSION_SAMPLINGBASED_KINODYNAMIC_CONTROL_
+
+#include <cstdlib>
+
+namespace ompl
+{
+    namespace sb
+    {
+	
+	/** Definition of a kinematic state: an array of doubles */
+	class Control
+	{
+	public:
+	    
+	    enum 
+		{
+		    NO_FLAGS       = 0,
+		    SELF_ALLOCATED = 1
+		};
+	    
+	    Control(void) : flags(NO_FLAGS)
+	    {
+		values = NULL;
+	    }
+	    
+	    Control(const unsigned int dimension) : flags(SELF_ALLOCATED)
+	    {
+		values = new double[dimension];
+	    }
+	    
+	    virtual ~Control(void)
+	    {
+		if ((flags & SELF_ALLOCATED) && values)
+		    delete[] values;
+	    }
+	    
+	    int     flags;
+	    double *values;
+	};
+	
+    }
+}
+
+#endif
