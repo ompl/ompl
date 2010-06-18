5e947b651ae8b2c6ca7c2880589ac9926842da0f
Mark Moll <mmoll@rice.edu>
2010-06-18 21:05:00 +0000

    Small changes.
    --HG--
    extra : convert_revision : 2c8138da0dac5ebd384fc93720b1475d71507657

--- ompl/CMakeModules/FindPython.cmake
+++ ompl/CMakeModules/FindPython.cmake
@@ -1,6 +1,31 @@
-find_program(PYTHON_EXEC "python${PYTHON_VERSION}" 
+# This code sets the following variables:
+# PYTHON_EXEC         - path to python executable
+# PYTHON_LIBRARIES    - path to the python library
+# PYTHON_INCLUDE_DIRS - path to where Python.h is found
+# PTYHON_SITE_MODULES - path to site-packages
+# PYTHON_ARCH         - name of architecture to be used for platform-specific 
+#                       binary modules
+# PYTHON_VERSION      - version of python
+#
+# You can optionally include the version number when using this package 
+# like so:
+#	find_package(python 2.6)
+#
+# This code defines a helper function find_python_module(). It can be used 
+# like so:
+#	find_python_module(numpy)
+# If numpy is found, the variable PY_NUMPY contains the location of the numpy 
+# module. If the module is required add the keyword "REQUIRED":
+#	find_python_module(numpy REQUIRED)
+
+find_program(PYTHON_EXEC "python${Python_FIND_VERSION}" 
 	DOC "Location of python executable to use")
-string(REGEX REPLACE "/bin/python${PYTHON_VERSION}$" "" PYTHON_PREFIX "${PYTHON_EXEC}")
+string(REGEX REPLACE "/bin/python${Python_FIND_VERSION}$" "" PYTHON_PREFIX
+	"${PYTHON_EXEC}")
+execute_process(COMMAND "${PYTHON_EXEC}" "-c"
+	"import sys; print '%d.%d' % (sys.version_info[0],sys.version_info[1])"
+	OUTPUT_VARIABLE PYTHON_VERSION
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
 find_library(PYTHON_LIBRARIES "python${PYTHON_VERSION}" PATHS "${PYTHON_PREFIX}" 
 	PATH_SUFFIXES "lib" "lib/python${PYTHON_VERSION}/config" 
 	DOC "Python libraries" NO_DEFAULT_PATH)
@@ -13,20 +38,24 @@ execute_process(COMMAND "${PYTHON_EXEC}" "-c"
 	OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 function(find_python_module module)
-	execute_process(COMMAND "${PYTHON_EXEC}" "-c" "import ${module}"
-		RESULT_VARIABLE _${module}_status ERROR_QUIET)
 	string(TOUPPER ${module} module_upper)
-	if(NOT ${module_upper}_FOUND)
-		if(_${module}_status)
-			set(${module_upper}_FOUND FALSE)
-			message(STATUS "Checking if Python module ${module} exists - no")
-		else(_${module}_status)
-			set(${module_upper}_FOUND TRUE)
-			message(STATUS "Checking if Python module ${module} exists - yes")
-		endif(_${module}_status)
-		set(${module_upper}_FOUND ${${module_upper}_FOUND} CACHE BOOL
-			"Whether Python module ${module} exists")
-	endif(NOT ${module_upper}_FOUND)
+	if(NOT PY_${module_upper})
+		if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
+			set(${module}_FIND_REQUIRED TRUE)
+		endif()
+		# A module's location is usually a directory, but for binary modules
+		# it's a .so file.
+		execute_process(COMMAND "${PYTHON_EXEC}" "-c" 
+			"import re, ${module}; print re.compile('/__init__.py.*').sub('',${module}.__file__)"
+			RESULT_VARIABLE _${module}_status 
+			OUTPUT_VARIABLE _${module}_location
+			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+		if(NOT _${module}_status)
+			set(PY_${module_upper} ${_${module}_location} CACHE STRING 
+				"Location of Python module ${module}")
+		endif(NOT _${module}_status)
+	endif(NOT PY_${module_upper})
+	find_package_handle_standard_args(PY_${module} DEFAULT_MSG PY_${module_upper})
 endfunction(find_python_module)
 
 set(PYTHON_ARCH "unknown")
@@ -48,4 +77,3 @@ endif(APPLE)
 
 find_package_handle_standard_args(Python DEFAULT_MSG 
 	PYTHON_LIBRARIES PYTHON_INCLUDE_DIRS PYTHON_SITE_MODULES)
-
