2fc10bfd7bd97414c3c3adf309900a12a16800e1
Mark Moll <mmoll@rice.edu>
2010-08-27 19:39:21 +0000

    dynamic lib loading fix for Linux
    --HG--
    extra : convert_revision : 6c101fa1ac38224e9909a8eab47162c9d35af606

--- trunk/py-bindings/ompl/__init__.py
+++ trunk/py-bindings/ompl/__init__.py
@@ -6,8 +6,11 @@ for lib in ['ompl', 'ompl_app']:
 	if libname:
 		ctypes.CDLL(libname, ctypes.RTLD_GLOBAL)
 	else:
+		from os.path import abspath, dirname
 		# find_library can't seem to find the libraries on Linux,
 		# even if the libs are in the current directory or
 		# if LD_LIBRARY_PATH is set to the directory where the libs
 		# reside. The call below still succeeds, though.
-		ctypes.CDLL('lib'+lib+'.so', ctypes.RTLD_GLOBAL)
+		ctypes.CDLL(
+			dirname(abspath(__file__))+'/lib'+lib+'.so',
+			ctypes.RTLD_GLOBAL)
