4ca66333d8d361a0c5c248ce0670979ecd7a5c38
Mark Moll <mmoll@rice.edu>
2010-08-27 14:40:41 +0000

    work around broken ctypes.util.find_library on Linux
    --HG--
    extra : convert_revision : 81061de0dfb80a1824ebb6fe00e0f4542a94f44a

--- trunk/py-bindings/ompl/__init__.py
+++ trunk/py-bindings/ompl/__init__.py
@@ -1,8 +1,13 @@
 import ctypes
 from ctypes.util import find_library
-ompl = find_library('ompl')
-ompl_app = find_library('ompl_app')
-if ompl:
-	ctypes.CDLL(ompl, ctypes.RTLD_GLOBAL)
-if ompl_app:
-	ctypes.CDLL(ompl_app, ctypes.RTLD_GLOBAL)
+
+for lib in ['ompl', 'ompl_app']:
+	libname = find_library(lib)
+	if libname:
+		ctypes.CDLL(libname, ctypes.RTLD_GLOBAL)
+	else:
+		# find_library can't seem to find the libraries on Linux,
+		# even if the libs are in the current directory or
+		# if LD_LIBRARY_PATH is set to the directory where the libs
+		# reside. The call below still succeeds, though.
+		ctypes.CDLL('lib'+lib+'.so', ctypes.RTLD_GLOBAL)
