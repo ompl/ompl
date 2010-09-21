d56e8340a98e58672adfb46377f7a3e71583412d
Mark Moll <mmoll@rice.edu>
2010-09-21 23:10:52 +0000

    replace OMPL-app with OMPL.app
    --HG--
    extra : convert_revision : 33b72ff880d5be8072cdf1a2aa5b13a260dba489

--- trunk/py-bindings/ompl/app/__init__.py
+++ trunk/py-bindings/ompl/app/__init__.py
@@ -4,5 +4,5 @@ try:
 except:
 	print """  The ompl.app module could not be imported. This module is only included
   with the OMPL-app front-end, not the library-only distribution. If you
-  downloaded the OMPL-app front-end, perhaps the ompl.app module did not
+  downloaded the OMPL.app front-end, perhaps the ompl.app module did not
   compile?"""
