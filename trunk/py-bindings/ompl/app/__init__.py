8a5787b4a193d167ca1ef1f4654415e71cba6588
Ioan Sucan <isucan@gmail.com>
2010-09-23 21:27:17 +0000

    removing forgotten folder
    --HG--
    extra : convert_revision : 860b98685d4e5c32f78f19510707091ee41112b7

--- trunk/py-bindings/ompl/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from ompl.geometric import *
-try:
-	from _app import *
-except:
-	print """  The ompl.app module could not be imported. This module is only included
-  with the OMPL-app front-end, not the library-only distribution. If you
-  downloaded the OMPL.app front-end, perhaps the ompl.app module did not
-  compile?"""
