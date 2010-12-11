847a861dd7caa07eb38cfd53c9772e681802f385
Ioan Sucan <isucan@gmail.com>
2010-12-11 02:16:26 +0000

    update make website script
    --HG--
    extra : convert_revision : a1176d2a997a03dcafee32fb9fb2352166dc61ca

--- trunk/doc/mkwebdocs.sh
+++ trunk/doc/mkwebdocs.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+BRANCH=trunk
+
 # web server
 export SERVER=arachne.cs.rice.edu
 # directory on web server where different versions of the web site 
 # and other assets are located.
-export ASSETS_ROOT=/mnt/data2/ompl/beta
-export ASSET_DIR=core
+export ASSETS_ROOT=/mnt/data2/ompl/$BRANCH
+export ASSET_DIR=ompl
+
+echo "Exporting OMPL documentation for $BRANCH"
 
 rm -rf ${ASSET_DIR}
 # copy all assets to the ASSET_DIR directory
@@ -16,7 +20,7 @@ done
 cp -r css js images html/*.png html/*.map html/search ${ASSET_DIR}
 
 # copy everything to web server and fix permissions
-tar cf - --exclude .svn ${ASSET_DIR} | ssh ${SERVER} 'cd '${ASSETS_ROOT}'; tar xf -; chmod -R a+rX '${ASSET_DIR}'; chgrp -R ompl '${ASSET_DIR}'; chmod -R g+w '${ASSET_DIR}
+tar cf - --exclude .svn ${ASSET_DIR} | ssh ${SERVER} 'mkdir -p '${ASSETS_ROOT}'; cd '${ASSETS_ROOT}'; tar xf -; chmod -R a+rX '${ASSET_DIR}'; chgrp -R ompl '${ASSET_DIR}'; chmod -R g+w '${ASSET_DIR}
 
 # clean up
 rm -rf ${ASSET_DIR}
