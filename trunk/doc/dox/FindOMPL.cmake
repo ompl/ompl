998be027560050d2c9394c9842bce39ab029c6ec
Ioan Sucan <isucan@gmail.com>
2010-12-12 18:57:25 +0000

    update var name in FindOMPL.cmake
    --HG--
    extra : convert_revision : 3fcfb5bfbdb24f297b8cecf92979c59a06e37c3f

--- trunk/doc/dox/FindOMPL.cmake
+++ trunk/doc/dox/FindOMPL.cmake
@@ -27,12 +27,12 @@ endif()
 find_library(OMPL_LIBRARY ompl PATHS ${OMPL_LIB_PATH} PATH_SUFFIXES lib build/lib)
 
 # find include path
-find_path(OMPL_INCLUDE SpaceInformation.h PATHS ${OMPL_INCLUDE_PATH} PATH_SUFFIXES base ompl/base include/ompl/base src/ompl/base)
-if (OMPL_INCLUDE)
-    string(REGEX REPLACE "/ompl/base$" "" OMPL_INCLUDE ${OMPL_INCLUDE})
+find_path(OMPL_INCLUDE_DIR SpaceInformation.h PATHS ${OMPL_INCLUDE_PATH} PATH_SUFFIXES base ompl/base include/ompl/base src/ompl/base)
+if (OMPL_INCLUDE_DIR)
+    string(REGEX REPLACE "/ompl/base$" "" OMPL_INCLUDE_DIR ${OMPL_INCLUDE_DIR})
 else()
-    set(OMPL_INCLUDE "")
+    set(OMPL_INCLUDE_DIR "")
 endif()
 
-find_package_handle_standard_args(OMPL DEFAULT_MSG OMPL_LIBRARY OMPL_INCLUDE)
-mark_as_advanced(OMPL_LIBRARY OMPL_INCLUDE)
+find_package_handle_standard_args(OMPL DEFAULT_MSG OMPL_LIBRARY OMPL_INCLUDE_DIR)
+mark_as_advanced(OMPL_LIBRARY OMPL_INCLUDE_DIR)
