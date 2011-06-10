f80f111a4f5790a7113a0f2d849c7a0acfb7cd39
Mark Moll <mmoll@rice.edu>
2011-06-10 10:24:43 -0500

    fix compiler warnings

--- tests/datastructures/pdf.cpp
+++ tests/datastructures/pdf.cpp
@@ -44,13 +44,13 @@ TEST(PDF, Simple)
     EXPECT_TRUE(p.empty());
     Element& e = p.add(0, 1.0);
     EXPECT_EQ(0, p.sample(0.5));
-    EXPECT_EQ(1, p.size());
+    EXPECT_EQ(1u, p.size());
     EXPECT_FALSE(p.empty());
     p.add(1, 0.0);
     p.add(2, 0.0);
     p.add(3, 0.0);
     EXPECT_EQ(0, p.sample(0.5));
-    EXPECT_EQ(4, p.size());
+    EXPECT_EQ(4u, p.size());
 
     p.clear();
     Element& e25 = p.add(0, 25);
@@ -87,7 +87,7 @@ TEST(PDF, Simple)
 
     p.remove(e6);
     // 25 50 15 10 30
-    EXPECT_EQ(5, p.size());
+    EXPECT_EQ(5u, p.size());
     EXPECT_EQ(5, p.sample(0.8));
     EXPECT_EQ(1, p.sample(0.2));
 
@@ -108,7 +108,7 @@ TEST(PDF, Simple)
     EXPECT_EQ(2, p.sample(1.0));
 
     p.remove(e15);
-    EXPECT_EQ(0, p.size());
+    EXPECT_EQ(0u, p.size());
     EXPECT_TRUE(p.empty());
 
     p.clear();
