97e448998fa54d97e212eb12076cd45563fa41b3
Matt Maly <mmaly@rice.edu>
2011-08-06 02:01:23 -0500

    added BSD license statements and fixed include guards to match style guide
    --HG--
    branch : syclop

--- src/ompl/control/planners/syclop/Syclop.h
+++ src/ompl/control/planners/syclop/Syclop.h
@@ -1,5 +1,41 @@
-#ifndef SYCLOP_H
-#define SYCLOP_H
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2011, Rice University
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Rice University nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Matt Maly */
+
+#ifndef OMPL_CONTROL_PLANNERS_SYCLOP_SYCLOP_
+#define OMPL_CONTROL_PLANNERS_SYCLOP_SYCLOP_
 
 #define BOOST_NO_HASH
 #include <boost/graph/dijkstra_shortest_paths.hpp>
@@ -16,10 +52,27 @@ namespace ompl
 {
     namespace control
     {
-        //TODO documentation, triangular decomposition, fit syntax to style guide
+        /**
+           @anchor Syclop
+
+           @par Short description
+           Syclop is a multi-layered planner that guides a low-level sampling-based tree planner
+           through a sequence of sequence of workspace regions from start to goal.
+
+           @par External documentation
+           E. Plaku, L.E. Kavraki, and M.Y. Vardi,
+           Motion Planning with Dynamics by a Synergistic Combination of Layers of Planning,
+           in <em>IEEE Transactions on Robotics</em>, 2010.<br>
+           <a href="http://kavrakilab.org/node/737">Abstract</a>
+        */
+
+        /** \brief Synergistic Combination of Layers of Planning.
+            This is a base class which requires a low-level planner. */
         class Syclop : public base::Planner
         {
-        public:
+        public: //TODO documentation, triangular decomposition, fit syntax to style guide
+            /** \brief Constructor. Requires a Decomposition,
+                which Syclop uses to create high-level guides. */
             Syclop(const SpaceInformationPtr& si, Decomposition& d, const std::string& name) : ompl::base::Planner(si, name),
                 siC_(si.get()), decomp(d), graph(decomp.getNumRegions()), covGrid(COVGRID_LENGTH, 2, d)
             {
