e132da5de1dfbf0ad830854a4ad34b1089b4ad2b
Mark Moll <mmoll@rice.edu>
2011-08-15 10:21:11 -0500

    minor fixes

--- src/ompl/geometric/planners/prm/PRM.h
+++ src/ompl/geometric/planners/prm/PRM.h
@@ -34,8 +34,8 @@
 
 /* Author: Ioan Sucan, James D. Marble */
 
-#ifndef OMPL_GEOMETRIC_PLANNERS_PRM_BASIC_PRM_
-#define OMPL_GEOMETRIC_PLANNERS_PRM_BASIC_PRM_
+#ifndef OMPL_GEOMETRIC_PLANNERS_PRM_PRM_
+#define OMPL_GEOMETRIC_PLANNERS_PRM_PRM_
 
 #include "ompl/geometric/planners/PlannerIncludes.h"
 #include "ompl/datastructures/NearestNeighbors.h"
