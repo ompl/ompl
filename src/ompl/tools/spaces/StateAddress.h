01e78766423481012c4d4dae6f300f466b0eec55
Ioan Sucan <isucan@willowgarage.com>
2011-10-13 17:45:49 -0700

    new tool for accessing state components faster, by name

---
+++ src/ompl/tools/spaces/StateAddress.h
@@ -0,0 +1,90 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2011, Willow Garage
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Ioan Sucan */
+
+#ifndef OMPL_TOOLS_SPACES_STATE_ADDRESS_
+#define OMPL_TOOLS_SPACES_STATE_ADDRESS_
+
+#include "ompl/base/StateSpace.h"
+#include "ompl/util/Console.h"
+#include <vector>
+#include <string>
+#include <map>
+
+namespace ompl
+{
+
+    class StateAddress
+    {
+    public:
+
+        struct Location
+        {
+            std::vector<std::size_t> chain;
+            const base::StateSpace  *space;
+            std::size_t              index;
+        };
+
+        StateAddress(const base::StateSpacePtr &space);
+
+        StateAddress(void);
+
+        ~StateAddress(void);
+
+        void setStateSpace(const base::StateSpacePtr &space);
+
+        const base::StateSpacePtr& getStateSpace(void) const
+        {
+            return space_;
+        }
+
+        double* getValueAddressAtLocation(const Location &loc, base::State *state) const;
+        const double* getValueAddressAtLocation(const Location &loc, const base::State *state) const;
+
+        double* getValueAddressAtName(const std::string &name, base::State *state) const;
+        const double* getValueAddressAtName(const std::string &name, const base::State *state) const;
+
+    private:
+
+        void storeLocation(Location &loc, const base::StateSpace *s);
+
+        base::StateSpacePtr             space_;
+        std::map<std::string, Location> locations_;
+
+        msg::Interface                  msg_;
+    };
+}
+
+#endif
