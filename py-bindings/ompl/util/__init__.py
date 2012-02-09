3fd3795eb1f055e93c74362dfa5bdf46a5141551
Ryan Luna <rluna@rice.edu>
2012-02-09 09:55:52 -0600

    Revert changes to py-bindings script
    --HG--
    branch : windows

--- py-bindings/ompl/util/__init__.py
+++ py-bindings/ompl/util/__init__.py
@@ -1,6 +1,4 @@
 from os.path import abspath, dirname
-from sys import platform
-if platform != 'nt' and platform != 'win32':
-    from ompl import dll_loader
-    dll_loader('ompl', dirname(abspath(__file__)))
+from ompl import dll_loader
+dll_loader('ompl', dirname(abspath(__file__)))
 from _util import *
