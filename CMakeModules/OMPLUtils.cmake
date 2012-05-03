501e4ae5a4e38ba4eef8ebf6cd5021aa3fc1ec5f
Ryan Luna <rluna@rice.edu>
2012-05-03 10:55:49 -0500

    Force dynamic linking with Boost.Test
    --HG--
    branch : boost_test

--- CMakeModules/OMPLUtils.cmake
+++ CMakeModules/OMPLUtils.cmake
@@ -6,8 +6,7 @@ macro(add_ompl_test test_name)
     ${Boost_SYSTEM_LIBRARY}
     ${Boost_THREAD_LIBRARY}
     ${Boost_DATE_TIME_LIBRARY}
-    ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
-    ${Boost_TEST_EXEC_MONITOR_LIBRARY})
+    ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
   add_test(${test_name} ${EXECUTABLE_OUTPUT_PATH}/${test_name})
 endmacro(add_ompl_test)
 
