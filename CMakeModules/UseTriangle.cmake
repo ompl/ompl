eeb61a6e2188f8c6fc3e766bb19e8cf0e1c9e3a5
Matt Maly <mmaly@rice.edu>
2012-05-06 22:03:46 -0500

    removed redundant commands from triangle cmake file
    --HG--
    branch : syclop-triangulate

--- CMakeModules/UseTriangle.cmake
+++ CMakeModules/UseTriangle.cmake
@@ -1,15 +1,3 @@
-include(FindPackageHandleStandardArgs)
-find_library (TRIANGLE_LIBRARY NAMES triangle TRIANGLE DOC "Location of the Triangle package")
-find_path (TRIANGLE_INCLUDE_DIR triangle.h PATH_SUFFIXES triangle)
-
-# TODO: Fail if triangle is not found, or perhaps compile without TriangularDecompositions
-if (TRIANGLE_LIBRARY AND TRIANGLE_INCLUDE_DIR)
-    find_package_handle_standard_args(triangle DEFAULT_MSG TRIANGLE_LIBRARY TRIANGLE_INCLUDE_DIR)
-    message (STATUS "Triangle package found.")
-else (TRIANGLE_LIBRARY AND TRIANGLE_INCLUDE_DIR)
-    message (STATUS "Triangle package not found.")
-endif (TRIANGLE_LIBRARY AND TRIANGLE_INCLUDE_DIR)
-
 find_library(TRIANGLE_LIBRARY triangle DOC "Location of Triangle library")
 find_path(TRIANGLE_INCLUDE_DIR triangle.h PATH_SUFFIXES triangle
    DOC "Location of Triangle header file directory")
@@ -19,5 +7,7 @@ if(TRIANGLE_LIBRARY AND TRIANGLE_INCLUDE_DIR)
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(triangle DEFAULT_MSG TRIANGLE_LIBRARY TRIANGLE_INCLUDE_DIR)
 else(TRIANGLE_LIBRARY AND TRIANGLE_INCLUDE_DIR)
+  #otherwise, print an error and exit
+  #TODO: if triangle not found, compile ompl without triangular decompositions
     message(FATAL_ERROR "Triangle is not found")
 endif(TRIANGLE_LIBRARY AND TRIANGLE_INCLUDE_DIR)
