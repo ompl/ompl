60c352848edc229c1eaa638c4817a7cfd1b6ca00
Mark Moll <mmoll@rice.edu>
2012-07-12 22:26:51 -0500

    enable versioned installs on Unix (including OS X)

--- CMakeModules/OMPLUtils.cmake
+++ CMakeModules/OMPLUtils.cmake
@@ -14,3 +14,30 @@ macro(add_ompl_python_test test_file)
   get_filename_component(test_name "${test_file}" NAME)
   add_test(${test_name} "${PYTHON_EXEC}" "${CMAKE_CURRENT_SOURCE_DIR}/${test_file}" "-v")
 endmacro(add_ompl_python_test)
+
+if(UNIX)
+    option(OMPL_VERSIONED_INSTALL "Append version suffix to binaries, libraries, and include dir." ON)
+else()
+    option(OMPL_VERSIONED_INSTALL "Append version suffix to binaries, libraries, and include dir." OFF)
+endif()
+
+if (OMPL_VERSIONED_INSTALL)
+    set(OMPL_INSTALL_SUFFIX "-${OMPL_MAJOR_VERSION}.${OMPL_MINOR_VERSION}")
+    macro(ompl_install_symlink path file)
+        get_filename_component(base ${file} NAME_WE)
+        string(REPLACE "${OMPL_VERSION}" "${OMPL_INSTALL_SUFFIX}" new_base "${base}")
+        get_filename_component(ext ${file} EXT)
+        install(CODE "execute_process(COMMAND mkdir -p \"${CMAKE_INSTALL_PREFIX}/${path}\"
+            COMMAND ln -sf
+            \"${CMAKE_INSTALL_PREFIX}/${path}/${new_base}${OMPL_INSTALL_SUFFIX}${ext}\"
+            \"${CMAKE_INSTALL_PREFIX}/${path}/${file}\")")
+            message("execute_process(COMMAND ln -sf
+            \"${CMAKE_INSTALL_PREFIX}/${path}/${new_base}${OMPL_INSTALL_SUFFIX}${ext}\"
+            \"${CMAKE_INSTALL_PREFIX}/${path}/${file}\")")
+    endmacro(ompl_install_symlink)
+else()
+    set(OMPL_INSTALL_SUFFIX "")
+    macro(ompl_install_symlink path file)
+        # do nothing
+    endmacro(ompl_install_symlink)
+endif()
