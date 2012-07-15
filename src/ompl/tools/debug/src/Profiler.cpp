b0e37160bc4196058151b3b5556096738a2ac98a
Ioan Sucan <isucan@willowgarage.com>
2012-07-15 16:07:53 -0700

    improve profiler output

--- src/ompl/tools/debug/src/Profiler.cpp
+++ src/ompl/tools/debug/src/Profiler.cpp
@@ -257,10 +257,14 @@ void ompl::tools::Profiler::printThreadInfo(std::ostream &out, const PerThread &
         out << std::endl;
         unaccounted -= time[i].value;
     }
-    out << "Unaccounted time : " << unaccounted;
-    if (total > 0.0)
-        out << " (" << (100.0 * unaccounted / total) << " %)";
-    out << std::endl;
+    // if we do not appear to have counted time multiple times, print the unaccounted time too
+    if (unaccounted >= 0.0)
+    {
+        out << "Unaccounted time : " << unaccounted;
+        if (total > 0.0)
+            out << " (" << (100.0 * unaccounted / total) << " %)";
+        out << std::endl;
+    }
 
     out << std::endl;
 }
