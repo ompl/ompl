190d119a9ed5d05c2604936615738d19caba97a7
Dave Coleman <dcoleman@willowgarage.com>
2012-07-23 22:55:46 -0700

    Added T-RRT Planner
    --HG--
    branch : costs

---
+++ src/ompl/geometric/planners/rrt/src/TRRT.cpp
@@ -0,0 +1,230 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Dave Coleman */
+
+#include "ompl/geometric/planners/rrt/TRRT.h"
+#include "ompl/base/goals/GoalSampleableRegion.h"
+#include "ompl/datastructures/NearestNeighborsGNAT.h"
+#include "ompl/tools/config/SelfConfig.h"
+#include <limits>
+
+ompl::geometric::TRRT::TRRT(const base::SpaceInformationPtr &si) : base::Planner(si, "TRRT")
+{
+    specs_.approximateSolutions = true;
+    specs_.directed = true;
+
+    goalBias_ = 0.05;
+    maxDistance_ = 0.0;
+    lastGoalMotion_ = NULL;
+
+    Planner::declareParam<double>("range", this, &TRRT::setRange, &TRRT::getRange);
+    Planner::declareParam<double>("goal_bias", this, &TRRT::setGoalBias, &TRRT::getGoalBias);
+}
+
+ompl::geometric::TRRT::~TRRT(void)
+{
+    freeMemory();
+}
+
+void ompl::geometric::TRRT::clear(void)
+{
+    Planner::clear();
+    sampler_.reset();
+    freeMemory();
+    if (nn_)
+        nn_->clear();
+    lastGoalMotion_ = NULL;
+}
+
+void ompl::geometric::TRRT::setup(void)
+{
+    Planner::setup();
+    tools::SelfConfig sc(si_, getName());
+    sc.configurePlannerRange(maxDistance_);
+
+    if (!nn_)
+        nn_.reset(new NearestNeighborsGNAT<Motion*>());
+    nn_->setDistanceFunction(boost::bind(&TRRT::distanceFunction, this, _1, _2));
+}
+
+void ompl::geometric::TRRT::freeMemory(void)
+{
+    if (nn_)
+    {
+        std::vector<Motion*> motions;
+        nn_->list(motions);
+        for (unsigned int i = 0 ; i < motions.size() ; ++i)
+        {
+            if (motions[i]->state)
+                si_->freeState(motions[i]->state);
+            delete motions[i];
+        }
+    }
+}
+
+ompl::base::PlannerStatus ompl::geometric::TRRT::solve(const base::PlannerTerminationCondition &ptc)
+{
+    checkValidity();
+    base::Goal                 *goal   = pdef_->getGoal().get();
+    base::GoalSampleableRegion *goal_s = dynamic_cast<base::GoalSampleableRegion*>(goal);
+
+    while (const base::State *st = pis_.nextStart())
+    {
+        Motion *motion = new Motion(si_);
+        si_->copyState(motion->state, st);
+        nn_->add(motion);
+    }
+
+    if (nn_->size() == 0)
+    {
+        logError("There are no valid initial states!");
+        return base::PlannerStatus::INVALID_START;
+    }
+
+    if (!sampler_)
+        sampler_ = si_->allocStateSampler();
+
+    logInform("Starting with %u states", nn_->size());
+
+    Motion *solution  = NULL;
+    Motion *approxsol = NULL;
+    double  approxdif = std::numeric_limits<double>::infinity();
+    Motion *rmotion   = new Motion(si_);
+    base::State *rstate = rmotion->state;
+    base::State *xstate = si_->allocState();
+
+    while (ptc() == false)
+    {
+
+        /* sample random state (with goal biasing) */
+        if (goal_s && rng_.uniform01() < goalBias_ && goal_s->canSample())
+            goal_s->sampleGoal(rstate);
+        else
+            sampler_->sampleUniform(rstate);
+
+        /* find closest state in the tree */
+        Motion *nmotion = nn_->nearest(rmotion);
+        base::State *dstate = rstate;
+
+        /* find state to add */
+        double d = si_->distance(nmotion->state, rstate);
+        if (d > maxDistance_)
+        {
+            si_->getStateSpace()->interpolate(nmotion->state, rstate, maxDistance_ / d, xstate);
+            dstate = xstate;
+        }
+
+        if (si_->checkMotion(nmotion->state, dstate))
+        {
+            /* create a motion */
+            Motion *motion = new Motion(si_);
+            si_->copyState(motion->state, dstate);
+            motion->parent = nmotion;
+
+            nn_->add(motion);
+            double dist = 0.0;
+            bool sat = goal->isSatisfied(motion->state, &dist);
+            if (sat)
+            {
+                approxdif = dist;
+                solution = motion;
+                break;
+            }
+            if (dist < approxdif)
+            {
+                approxdif = dist;
+                approxsol = motion;
+            }
+        }
+    }
+
+    bool solved = false;
+    bool approximate = false;
+    if (solution == NULL)
+    {
+        solution = approxsol;
+        approximate = true;
+    }
+
+    if (solution != NULL)
+    {
+        lastGoalMotion_ = solution;
+
+        /* construct the solution path */
+        std::vector<Motion*> mpath;
+        while (solution != NULL)
+        {
+            mpath.push_back(solution);
+            solution = solution->parent;
+        }
+
+        /* set the solution path */
+        PathGeometric *path = new PathGeometric(si_);
+           for (int i = mpath.size() - 1 ; i >= 0 ; --i)
+            path->append(mpath[i]->state);
+        pdef_->addSolutionPath(base::PathPtr(path), approximate, approxdif);
+        solved = true;
+    }
+
+    si_->freeState(xstate);
+    if (rmotion->state)
+        si_->freeState(rmotion->state);
+    delete rmotion;
+
+    logInform("Created %u states", nn_->size());
+
+    return base::PlannerStatus(solved, approximate);
+}
+
+void ompl::geometric::TRRT::getPlannerData(base::PlannerData &data) const
+{
+    Planner::getPlannerData(data);
+
+    std::vector<Motion*> motions;
+    if (nn_)
+        nn_->list(motions);
+
+    if (lastGoalMotion_)
+        data.addGoalVertex(base::PlannerDataVertex(lastGoalMotion_->state));
+
+    for (unsigned int i = 0 ; i < motions.size() ; ++i)
+    {
+        if (motions[i]->parent == NULL)
+            data.addStartVertex(base::PlannerDataVertex(motions[i]->state));
+        else
+            data.addEdge(base::PlannerDataVertex(motions[i]->parent->state),
+                         base::PlannerDataVertex(motions[i]->state));
+    }
+}
--- src/ompl/geometric/src/SimpleSetup.cpp
+++ src/ompl/geometric/src/SimpleSetup.cpp
@@ -38,204 +38,205 @@
 #include "ompl/base/goals/GoalSampleableRegion.h"
 #include "ompl/geometric/planners/rrt/RRTConnect.h"
 #include "ompl/geometric/planners/rrt/RRT.h"
+#include "ompl/geometric/planners/rrt/TRRT.h"
 #include "ompl/geometric/planners/kpiece/LBKPIECE1.h"
 #include "ompl/geometric/planners/kpiece/KPIECE1.h"
 
 ompl::base::PlannerPtr ompl::geometric::getDefaultPlanner(const base::GoalPtr &goal)
 {
-  base::PlannerPtr planner;
-  if (!goal)
-    throw Exception("Unable to allocate default planner for unspecified goal definition");
+    base::PlannerPtr planner;
+    if (!goal)
+        throw Exception("Unable to allocate default planner for unspecified goal definition");
 
-  // if we can sample the goal region, use a bi-directional planner
-  if (goal->hasType(base::GOAL_SAMPLEABLE_REGION))
+    // if we can sample the goal region, use a bi-directional planner
+    if (goal->hasType(base::GOAL_SAMPLEABLE_REGION) && false ) // TODO: remove this false
     {
-      // if we have a default projection
-      if (goal->getSpaceInformation()->getStateSpace()->hasDefaultProjection())
-	{
-	  logInform("Using LBKPIECE1 planner");
-	  planner = base::PlannerPtr(new LBKPIECE1(goal->getSpaceInformation()));
-	}
-      else
-	{
-	  logInform("Using RRTConnect planner");
-	  planner = base::PlannerPtr(new RRTConnect(goal->getSpaceInformation()));
-	}
+        // if we have a default projection
+        if (goal->getSpaceInformation()->getStateSpace()->hasDefaultProjection())
+        {
+            logInform("Using LBKPIECE1 planner 1");
+            planner = base::PlannerPtr(new LBKPIECE1(goal->getSpaceInformation()));
+        }
+        else
+        {
+            logInform("Using RRTConnect planner");
+            planner = base::PlannerPtr(new RRTConnect(goal->getSpaceInformation()));
+        }
     }
-  // other use a single-tree planner
-  else
+    // otherwise use a single-tree planner
+    else
     {
-      // if we have a default projection
-      if (goal->getSpaceInformation()->getStateSpace()->hasDefaultProjection())
-	{
-	  logInform("Using KPIECE1 planner");
-	  planner = base::PlannerPtr(new KPIECE1(goal->getSpaceInformation()));
-	}
-      else
-	{
-	  logInform("Using RRT planner");
-	  planner = base::PlannerPtr(new RRT(goal->getSpaceInformation()));
-	}
+        // if we have a default projection
+        if (goal->getSpaceInformation()->getStateSpace()->hasDefaultProjection() && false) //TODO: remove this false
+        {
+            logInform("Using KPIECE1 planner 2");
+            planner = base::PlannerPtr(new KPIECE1(goal->getSpaceInformation()));
+        }
+        else
+        {
+            logInform("Using TRRT planner");
+            planner = base::PlannerPtr(new TRRT(goal->getSpaceInformation()));
+        }
     }
 
-  if (!planner)
-    throw Exception("Unable to allocate default planner");
+    if (!planner)
+        throw Exception("Unable to allocate default planner");
 
-  return planner;
+    return planner;
 }
 
 ompl::geometric::SimpleSetup::SimpleSetup(const base::StateSpacePtr &space) :
-  configured_(false), planTime_(0.0), simplifyTime_(0.0), invalid_request_(false)
+    configured_(false), planTime_(0.0), simplifyTime_(0.0), invalid_request_(false)
 {
-  si_.reset(new base::SpaceInformation(space));
-  pdef_.reset(new base::ProblemDefinition(si_));
-  psk_.reset(new PathSimplifier(si_));
-  params_.include(si_->params());
+    si_.reset(new base::SpaceInformation(space));
+    pdef_.reset(new base::ProblemDefinition(si_));
+    psk_.reset(new PathSimplifier(si_));
+    params_.include(si_->params());
 }
 
 void ompl::geometric::SimpleSetup::setup(void)
 {
-  if (!configured_ || !si_->isSetup() || !planner_->isSetup())
+    if (!configured_ || !si_->isSetup() || !planner_->isSetup())
     {
-      if (!si_->isSetup())
-	si_->setup();
-      if (!planner_)
+        if (!si_->isSetup())
+            si_->setup();
+        if (!planner_)
         {
-	  if (pa_)
-	    planner_ = pa_(si_);
-	  if (!planner_)
+            if (pa_)
+                planner_ = pa_(si_);
+            if (!planner_)
             {
-	      logInform("No planner specified. Using default. (geometric)");
-	      planner_ = getDefaultPlanner(getGoal());
+                logInform("No planner specified. Using default. (geometric fork)");
+                planner_ = getDefaultPlanner(getGoal());
             }
         }
-      planner_->setProblemDefinition(pdef_);
-      if (!planner_->isSetup())
-	planner_->setup();
+        planner_->setProblemDefinition(pdef_);
+        if (!planner_->isSetup())
+            planner_->setup();
 
-      params_.clear();
-      params_.include(si_->params());
-      params_.include(planner_->params());
-      configured_ = true;
+        params_.clear();
+        params_.include(si_->params());
+        params_.include(planner_->params());
+        configured_ = true;
     }
 }
 
 void ompl::geometric::SimpleSetup::clear(void)
 {
-  if (planner_)
-    planner_->clear();
-  if (pdef_)
-    pdef_->clearSolutionPaths();
+    if (planner_)
+        planner_->clear();
+    if (pdef_)
+        pdef_->clearSolutionPaths();
 }
 
 ompl::base::PlannerStatus ompl::geometric::SimpleSetup::solve(double time)
 {
-  setup();
-  invalid_request_ = false;
-  time::point start = time::now();
-  base::PlannerStatus result = planner_->solve(time);
-  planTime_ = time::seconds(time::now() - start);
-  if (result)
-    logInform("Solution found in %f seconds", planTime_);
-  else
+    setup();
+    invalid_request_ = false;
+    time::point start = time::now();
+    base::PlannerStatus result = planner_->solve(time);
+    planTime_ = time::seconds(time::now() - start);
+    if (result)
+        logInform("Solution found in %f seconds", planTime_);
+    else
     {
-      if (planTime_ < time)
-	invalid_request_ = true;
-      logInform("No solution found after %f seconds", planTime_);
+        if (planTime_ < time)
+            invalid_request_ = true;
+        logInform("No solution found after %f seconds", planTime_);
     }
-  return result;
+    return result;
 }
 
 ompl::base::PlannerStatus ompl::geometric::SimpleSetup::solve(const base::PlannerTerminationCondition &ptc)
 {
-  setup();
-  invalid_request_ = false;
-  time::point start = time::now();
-  base::PlannerStatus result = planner_->solve(ptc);
-  planTime_ = time::seconds(time::now() - start);
-  if (result)
-    logInform("Solution found in %f seconds", planTime_);
-  else
+    setup();
+    invalid_request_ = false;
+    time::point start = time::now();
+    base::PlannerStatus result = planner_->solve(ptc);
+    planTime_ = time::seconds(time::now() - start);
+    if (result)
+        logInform("Solution found in %f seconds", planTime_);
+    else
     {
-      if (!ptc())
-	invalid_request_ = true;
-      logInform("No solution found after %f seconds", planTime_);
+        if (!ptc())
+            invalid_request_ = true;
+        logInform("No solution found after %f seconds", planTime_);
     }
-  return result;
+    return result;
 }
 
 void ompl::geometric::SimpleSetup::simplifySolution(const base::PlannerTerminationCondition &ptc)
 {
-  if (pdef_)
+    if (pdef_)
     {
-      const base::PathPtr &p = pdef_->getSolutionPath();
-      if (p)
+        const base::PathPtr &p = pdef_->getSolutionPath();
+        if (p)
         {
-	  time::point start = time::now();
-	  psk_->simplify(static_cast<PathGeometric&>(*p), ptc);
-	  simplifyTime_ = time::seconds(time::now() - start);
-	  logInform("Path simplification took %f seconds", simplifyTime_);
-	  return;
+            time::point start = time::now();
+            psk_->simplify(static_cast<PathGeometric&>(*p), ptc);
+            simplifyTime_ = time::seconds(time::now() - start);
+            logInform("Path simplification took %f seconds", simplifyTime_);
+            return;
         }
     }
-  logWarn("No solution to simplify");
+    logWarn("No solution to simplify");
 }
 
 void ompl::geometric::SimpleSetup::simplifySolution(double duration)
 {
-  if (pdef_)
+    if (pdef_)
     {
-      const base::PathPtr &p = pdef_->getSolutionPath();
-      if (p)
+        const base::PathPtr &p = pdef_->getSolutionPath();
+        if (p)
         {
-	  time::point start = time::now();
-	  if (duration < std::numeric_limits<double>::epsilon())
-	    psk_->simplifyMax(static_cast<PathGeometric&>(*p));
-	  else
-	    psk_->simplify(static_cast<PathGeometric&>(*p), duration);
-	  simplifyTime_ = time::seconds(time::now() - start);
-	  logInform("Path simplification took %f seconds", simplifyTime_);
-	  return;
+            time::point start = time::now();
+            if (duration < std::numeric_limits<double>::epsilon())
+                psk_->simplifyMax(static_cast<PathGeometric&>(*p));
+            else
+                psk_->simplify(static_cast<PathGeometric&>(*p), duration);
+            simplifyTime_ = time::seconds(time::now() - start);
+            logInform("Path simplification took %f seconds", simplifyTime_);
+            return;
         }
     }
-  logWarn("No solution to simplify");
+    logWarn("No solution to simplify");
 }
 
 ompl::geometric::PathGeometric& ompl::geometric::SimpleSetup::getSolutionPath(void) const
 {
-  if (pdef_)
+    if (pdef_)
     {
-      const base::PathPtr &p = pdef_->getSolutionPath();
-      if (p)
-	return static_cast<PathGeometric&>(*p);
+        const base::PathPtr &p = pdef_->getSolutionPath();
+        if (p)
+            return static_cast<PathGeometric&>(*p);
     }
-  throw Exception("No solution path");
+    throw Exception("No solution path");
 }
 
 bool ompl::geometric::SimpleSetup::haveExactSolutionPath(void) const
 {
-  return haveSolutionPath() && (!pdef_->hasApproximateSolution() || pdef_->getSolutionDifference() < std::numeric_limits<double>::epsilon());
+    return haveSolutionPath() && (!pdef_->hasApproximateSolution() || pdef_->getSolutionDifference() < std::numeric_limits<double>::epsilon());
 }
 
 void ompl::geometric::SimpleSetup::getPlannerData(base::PlannerData &pd) const
 {
-  pd.clear();
-  if (planner_)
-    planner_->getPlannerData(pd);
+    pd.clear();
+    if (planner_)
+        planner_->getPlannerData(pd);
 }
 
 void ompl::geometric::SimpleSetup::print(std::ostream &out) const
 {
-  if (si_)
+    if (si_)
     {
-      si_->printProperties(out);
-      si_->printSettings(out);
+        si_->printProperties(out);
+        si_->printSettings(out);
     }
-  if (planner_)
+    if (planner_)
     {
-      planner_->printProperties(out);
-      planner_->printSettings(out);
+        planner_->printProperties(out);
+        planner_->printSettings(out);
     }
-  if (pdef_)
-    pdef_->print(out);
+    if (pdef_)
+        pdef_->print(out);
 }
