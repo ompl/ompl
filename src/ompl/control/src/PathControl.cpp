73457e5cbce058008579351b4a0d935bba7b2168
Luis G. Torres <lgtorres42@gmail.com>
2013-01-15 20:16:09 -0500

    Removing some useless commented code
    --HG--
    branch : cost_updates_luis
    extra : transplant_source : %3D%3D%827%A4H%E5%0E%11ej%CAy%AC%C5_%BF%AFH%28

--- src/ompl/control/src/PathControl.cpp
+++ src/ompl/control/src/PathControl.cpp
@@ -91,16 +91,6 @@ double ompl::control::PathControl::length(void) const
     return std::accumulate(controlDurations_.begin(), controlDurations_.end(), 0.0);
 }
 
-// double ompl::control::PathControl::cost(const base::OptimizationObjective &objective) const
-// {
-//     double L = 0.0;
-//     for (unsigned int i = 1 ; i < states_.size() ; ++i)
-//         L = objective.combineObjectiveCosts(L, objective.getIncrementalCost(states_[i-1], states_[i]));
-//     if (!states_.empty())
-//         L += objective.getTerminalCost(states_.back());
-//     return L;
-// }
-
 void ompl::control::PathControl::print(std::ostream &out) const
 {
     const SpaceInformation *si = static_cast<const SpaceInformation*>(si_.get());
