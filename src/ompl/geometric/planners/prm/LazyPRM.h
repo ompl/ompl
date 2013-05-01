ae447a7caca855c1ffebf4967590f0ca5034a8c6
Ioan Sucan <isucan@willowgarage.com>
2013-05-01 20:50:38 +0300

    update docs
    --HG--
    branch : lazy_prm

--- src/ompl/geometric/planners/prm/LazyPRM.h
+++ src/ompl/geometric/planners/prm/LazyPRM.h
@@ -1,7 +1,7 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
 *
-*  Copyright (c) 2011, Rice University
+*  Copyright (c) 2013, Rice University
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
