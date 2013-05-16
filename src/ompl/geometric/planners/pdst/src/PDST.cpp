49638b9295ba67057bd11b19e85bac08ee9cf0ae
Mark Moll <mmoll@rice.edu>
2013-05-16 09:39:54 -0500

    cleaner version of PDST that is much closer to the pseudo-code in Andrew Ladd's thesis

--- src/ompl/geometric/planners/pdst/src/PDST.cpp
+++ src/ompl/geometric/planners/pdst/src/PDST.cpp
@@ -38,8 +38,8 @@
 #include "ompl/geometric/planners/pdst/PDST.h"
 
 ompl::geometric::PDST::PDST(const base::SpaceInformationPtr &si)
-    : base::Planner(si, "PDST"), bsp_(NULL),
-    goalBias_(0.05), goalSampler_(NULL), iteration_(1), lastGoalMotion_(NULL)
+    : base::Planner(si, "PDST"), bsp_(NULL), goalBias_(0.05),
+    goalSampler_(NULL), iteration_(1), lastGoalMotion_(NULL)
 {
     Planner::declareParam<double>("goal_bias", this, &PDST::setGoalBias, &PDST::getGoalBias, "0.:.05:1.");
 }
@@ -77,7 +77,7 @@ ompl::base::PlannerStatus ompl::geometric::PDST::solve(const base::PlannerTermin
     // time then initializes hasSolution to false and isApproximate to true.
     double distanceToGoal, closestDistanceToGoal = std::numeric_limits<double>::infinity();
     bool hasSolution = lastGoalMotion_ != NULL;
-    bool isApproximate = hasSolution ? goalSampler_->isSatisfied(lastGoalMotion_->state_, &closestDistanceToGoal) : true;
+    bool isApproximate = !hasSolution || !goal->isSatisfied(lastGoalMotion_->endState_, &closestDistanceToGoal);
     unsigned ndim = projectionEvaluator_->getDimension();
 
     // If an exact solution has already been found, do not run for another iteration.
@@ -87,7 +87,7 @@ ompl::base::PlannerStatus ompl::geometric::PDST::solve(const base::PlannerTermin
     // Initialize tree with start state(s)
     while (const base::State *st = pis_.nextStart())
     {
-        Motion *startMotion = new Motion(si_->cloneState(st), projectionEvaluator_);
+        Motion *startMotion = new Motion(si_->cloneState(st));
         bsp_->addMotion(startMotion);
         startMotion->heapElement_ = priorityQueue_.insert(startMotion);
     }
@@ -98,8 +98,8 @@ ompl::base::PlannerStatus ompl::geometric::PDST::solve(const base::PlannerTermin
         return base::PlannerStatus::INVALID_START;
     }
 
-    base::State* state = si_->allocState();
-    base::EuclideanProjection projection(projectionEvaluator_->getDimension());
+    base::State* tmpState1 = si_->allocState(), *tmpState2 = si_->allocState();
+    base::EuclideanProjection tmpProj(ndim);
     while (!ptc)
     {
         // Get the top priority path.
@@ -107,10 +107,12 @@ ompl::base::PlannerStatus ompl::geometric::PDST::solve(const base::PlannerTermin
         motionSelected->updatePriority();
         priorityQueue_.update(motionSelected->heapElement_);
 
-        Motion *newMotion = propagateFrom(motionSelected, state, projection);
+        Motion *newMotion = propagateFrom(motionSelected, tmpState1, tmpState2);
+        addMotion(newMotion, bsp_, tmpState1, tmpProj);
 
         // Check if the newMotion reached the goal.
-        if (goal->isSatisfied(newMotion->state_, &distanceToGoal))
+        hasSolution = goal->isSatisfied(newMotion->endState_, &distanceToGoal);
+        if (hasSolution)
         {
             closestDistanceToGoal = distanceToGoal;
             lastGoalMotion_ = newMotion;
@@ -131,11 +133,8 @@ ompl::base::PlannerStatus ompl::geometric::PDST::solve(const base::PlannerTermin
         cellSelected->subdivide(ndim);
         motions.swap(cellSelected->motions_);
         for (std::vector<Motion*>::iterator m = motions.begin() ; m != motions.end() ; ++m)
-            insertSampleIntoBsp(*m, cellSelected, state, projection);
-        iteration_++;
+            addMotion(*m, cellSelected, tmpState1, tmpProj);
     }
-    si_->freeState(state);
-
 
     if (lastGoalMotion_ != NULL)
         hasSolution = true;
@@ -146,125 +145,92 @@ ompl::base::PlannerStatus ompl::geometric::PDST::solve(const base::PlannerTermin
         PathGeometric *path = new PathGeometric(si_);
 
         // Compute the path by going up the tree of motions.
-        std::vector<Motion*> mpath;
-        Motion *solution = lastGoalMotion_;
-        while (solution->parent_ != NULL)
+        std::vector<base::State*> spath(1,  lastGoalMotion_->endState_);
+        Motion *m = lastGoalMotion_;
+        while (m)
         {
-            mpath.push_back(solution);
-            solution = solution->parent_;
+            m = m->ancestor();
+            spath.push_back(m->startState_);
+            m = m->parent_;
         }
 
         // Add the solution path in order from the start state to the goal.
-        for (std::vector<Motion*>::reverse_iterator rIt = mpath.rbegin(); rIt != mpath.rend(); ++rIt)
-            path->append((*rIt)->state_);
+        for (std::vector<base::State*>::reverse_iterator rIt = spath.rbegin(); rIt != spath.rend(); ++rIt)
+            path->append((*rIt));
         pdef_->addSolutionPath(base::PathPtr(path), isApproximate, closestDistanceToGoal);
     }
+
+    si_->freeState(tmpState1);
+    si_->freeState(tmpState2);
 
     return base::PlannerStatus(hasSolution, isApproximate);
 }
 
-ompl::geometric::PDST::Motion* ompl::geometric::PDST::propagateFrom(Motion* motion, base::State* state, base::EuclideanProjection& projection)
+ompl::geometric::PDST::Motion* ompl::geometric::PDST::propagateFrom(
+    Motion* motion, base::State* start, base::State* rnd)
 {
-    if (motion->parent_)
-    {
-        // sample a point along the trajectory given by motion
-        double length = rng_.uniform01();
-        // split motion into two motions
-        motion = motion->split(si_, length, projectionEvaluator_);
-        motion->heapElement_ = priorityQueue_.insert(motion);
-    }
-
+    // sample a point along the trajectory given by motion
+    si_->getStateSpace()->interpolate(motion->startState_, motion->endState_, rng_.uniform01(), start);
     // generate a random state
     if (goalSampler_ && rng_.uniform01() < goalBias_ && goalSampler_->canSample())
-        goalSampler_->sampleGoal(state);
+        goalSampler_->sampleGoal(rnd);
     else
-        sampler_->sampleUniform(state);
-
-    std::pair<base::State*, double> lastValid = std::make_pair(state, 0.);
-    si_->checkMotion(motion->state_, state, lastValid);
-    projectionEvaluator_->project(state, projection);
-
-    Motion* m = new Motion(si_->cloneState(state), motion, projection, iteration_, bsp_->stab(projection));
-    int numSegments = si_->getStateSpace()->validSegmentCount(motion->state_, m->state_);
-
-    m->heapElement_ = priorityQueue_.insert(m);
-    m->cell_->motions_.push_back(m);
-
-    if (numSegments > 1)
-    {
-        si_->getStateSpace()->interpolate(motion->state_, m->state_, 1.0 / (double)numSegments, state);
-        projectionEvaluator_->project(state, projection);
-        Cell* cell = bsp_->stab(projection);
-        while (cell != motion->cell_ && numSegments > 1)
-            cell = split(bsp_, cell, m, numSegments, state, projection);
-    }
-    return m;
+        sampler_->sampleUniform(rnd);
+    // compute longest valid segment from start towards rnd
+    std::pair<base::State*, double> lastValid = std::make_pair(rnd, 0.);
+    si_->checkMotion(start, rnd, lastValid);
+    return new Motion(si_->cloneState(start), si_->cloneState(rnd), ++iteration_, motion);
 }
 
-void ompl::geometric::PDST::insertSampleIntoBsp(Motion *motion, Cell *bsp, base::State* state, base::EuclideanProjection& projection)
+void ompl::geometric::PDST::addMotion(Motion *motion, Cell *bsp, base::State* state, base::EuclideanProjection& proj)
 {
-    bsp->stab(motion->projection_)->addMotion(motion);
-    priorityQueue_.update(motion->heapElement_);
+    projectionEvaluator_->project(motion->endState_, proj);
+    bsp->stab(proj)->addMotion(motion);
+    updateHeapElement(motion);
 
     // If the motion corresponds to a start state, then it cannot be split across cell
     // bounds. So we're done.
     if (!motion->parent_)
         return;
 
-    // if cell of this motion and its parent are the same, we are done
-    Cell* cell = motion->parent_->cell_;
-    if (cell == bsp)
-        cell = bsp->stab(motion->parent_->projection_);
-    if (cell == motion->cell_)
-        return;
+    // Otherwise, split into smaller segments s.t. endpoints project into same cell
+    int numSegments = si_->getStateSpace()->validSegmentCount(motion->startState_, motion->endState_);
+    Cell *startCell;
+    projectionEvaluator_->project(motion->startState_, proj);
+    startCell = bsp->stab(proj);
 
-    // motion can cross at most one cell boundary, since bsp has just been split
-    // into two cells.
-    int numSegments = si_->getStateSpace()->validSegmentCount(motion->parent_->state_, motion->state_);
-
-    if (numSegments > 1)
+    while (startCell != motion->cell_ && numSegments > 1)
     {
-        base::EuclideanProjection projection(projectionEvaluator_->getDimension());
-        si_->getStateSpace()->interpolate(motion->parent_->state_, motion->state_, 1.0 / (double)numSegments, state);
-        projectionEvaluator_->project(state, projection);
-        cell = bsp->stab(projection);
-        if (cell != motion->cell_)
-            split(bsp, cell, motion, numSegments, state, projection);
-    }
-}
-
-ompl::geometric::PDST::Cell* ompl::geometric::PDST::split(const Cell* bsp, Cell* startCell, Motion* motion, int& numSegments, base::State* state, base::EuclideanProjection& projection)
-{
-    Cell* nextStartCell = motion->cell_;
-    Cell* cell;
-    int i = 0, j = numSegments, k = 1;
-    // Find the largest k such that the interpolated state at k/numSegments is
-    // still in the startCell. The variables i and j bracket the range that k
-    // can be in. Initially, k=1. The loop will always be entered, because it
-    // is assumed that numSegments>1 (this is checked for in the calling
-    // contexts) and, hence, intially j-i>1.
-    while (j - i > 1)
-    {
-        k = (i + j) / 2;
-        si_->getStateSpace()->interpolate(motion->parent_->state_, motion->state_, (double)k / (double)numSegments, state);
-        projectionEvaluator_->project(state, projection);
-        cell = bsp->stab(projection);
-        if (cell == startCell)
-            i = k;
-        else
+        Cell *nextStartCell = motion->cell_, *cell;
+        int i = 0, j = numSegments, k;
+        // Find the largest k such that the interpolated state at k/numSegments is
+        // still in startCell. The variables i and j bracket the range that k
+        // can be in.
+        while (j - i > 1)
         {
-            j = k;
-            nextStartCell = cell;
+            k = (i + j) / 2;
+            si_->getStateSpace()->interpolate(motion->startState_, motion->endState_,
+                (double)k / (double)numSegments, state);
+            projectionEvaluator_->project(state, proj);
+            cell = bsp->stab(proj);
+            if (cell == startCell)
+                i = k;
+            else
+            {
+                j = k;
+                nextStartCell = cell;
+            }
         }
-    }
 
-    Motion* m = new Motion(si_->cloneState(state), motion->parent_, projection, iteration_, startCell);
-    motion->parent_ = m;
-    m->heapElement_ = priorityQueue_.insert(m);
-    m->cell_->motions_.push_back(m);
-    numSegments -= k;
-    // return the cell of the interpolated state at (k+1)/numSegments.
-    return nextStartCell;
+        Motion* m = new Motion(motion->startState_, si_->cloneState(state), motion->priority_, motion->parent_);
+        startCell->addMotion(m);
+        m->heapElement_ = priorityQueue_.insert(m);
+        m->isSplit_ = true;
+        motion->startState_ = m->endState_;
+        motion->parent_ = m;
+        numSegments -= k;
+        startCell = nextStartCell;
+    }
 }
 
 void ompl::geometric::PDST::clear(void)
@@ -274,7 +240,7 @@ void ompl::geometric::PDST::clear(void)
     iteration_ = 1;
     lastGoalMotion_ = NULL;
     freeMemory();
-    bsp_ = new Cell(1.0, projectionEvaluator_->getBounds(), 0);
+    bsp_ = new Cell(1., projectionEvaluator_->getBounds(), 0);
 }
 
 void ompl::geometric::PDST::freeMemory(void)
@@ -297,7 +263,7 @@ void ompl::geometric::PDST::setup(void)
     sc.configureProjectionEvaluator(projectionEvaluator_);
     if (bsp_)
         delete bsp_;
-    bsp_ = new Cell(1.0, projectionEvaluator_->getBounds(), 0);
+    bsp_ = new Cell(1., projectionEvaluator_->getBounds(), 0);
     lastGoalMotion_ = NULL;
 }
 
@@ -310,26 +276,31 @@ void ompl::geometric::PDST::getPlannerData(ompl::base::PlannerData &data) const
 
     // Add goal vertex
     if (lastGoalMotion_ != NULL)
-        data.addGoalVertex(lastGoalMotion_->state_);
+        data.addGoalVertex(lastGoalMotion_->endState_);
 
     for (std::vector<Motion*>::iterator it = motions.begin(); it < motions.end(); ++it)
-    {
-        Motion *child = *it;
-        Motion *parent = child->parent_;
-
-        if (parent == NULL)
-            data.addStartVertex(base::PlannerDataVertex(child->state_));
-        else
-            data.addEdge(base::PlannerDataVertex(parent->state_),
-                base::PlannerDataVertex(child->state_));
-    }
+        if (!(*it)->isSplit_)
+        {
+            Motion *cur = *it, *ancestor = cur->ancestor();
+            if (!cur->parent_)
+                data.addStartVertex(base::PlannerDataVertex(cur->endState_));
+            else
+            {
+                data.addEdge(base::PlannerDataVertex(ancestor->startState_),
+                    base::PlannerDataVertex(cur->endState_));
+                // add edge connecting start state of ancestor's parent to start state of ancestor
+                if (ancestor->parent_)
+                    data.addEdge(base::PlannerDataVertex(ancestor->parent_->ancestor()->startState_),
+                        base::PlannerDataVertex(ancestor->startState_));
+            }
+        }
 }
 
 void ompl::geometric::PDST::Cell::subdivide(unsigned int spaceDimension)
 {
-    double childVolume = 0.5 * volume_;
+    double childVolume = .5 * volume_;
     unsigned int nextSplitDimension = (splitDimension_ + 1) % spaceDimension;
-    splitValue_ = 0.5 * (bounds_.low[splitDimension_] + bounds_.high[splitDimension_]);
+    splitValue_ = .5 * (bounds_.low[splitDimension_] + bounds_.high[splitDimension_]);
 
     left_ = new Cell(childVolume, bounds_, nextSplitDimension);
     left_->bounds_.high[splitDimension_] = splitValue_;
