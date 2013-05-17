b470c0c0f7d1cff52be50fffb8fc41f525eaeff2
Ioan Sucan <isucan@willowgarage.com>
2013-05-17 15:57:47 +0300

    improvements to batch test script

--- tests/test_loop.sh
+++ tests/test_loop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+control_c()
+# run if user hits control-c
+{
+  echo -en "\n*** User requested termination ***\n"
+  exit 1
+}
+
 # This script simply runs a test many times in gdb
 TEST_EXE=../build/bin/test_2dmap_geometric
 
@@ -9,9 +16,18 @@ then
     COUNT=$1
 fi
 
+trap control_c SIGINT
+rm -f ompl_run_test_codes
+
 for i in $(seq $COUNT)
 do
     echo "Running test $i of $COUNT" > ompl_run_test_count
-    gdb --batch --ex run --args $TEST_EXE
+    gdb --return-child-result --batch --ex run --args $TEST_EXE
+    RET_CODE=$?
+    echo $RET_CODE >> ompl_run_test_codes
+    if [ $RET_CODE -ne 0 ]
+    then
+	echo "Error found!"
+    fi
 done
-rm ompl_run_test_count
+rm -f ompl_run_test_count
