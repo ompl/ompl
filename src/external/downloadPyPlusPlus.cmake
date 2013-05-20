e04a0e1a13726435d30410d19b427e302695f305
Mark Moll <mmoll@rice.edu>
2013-05-19 22:40:30 -0500

    update gccxml snapshot to fix support for gcc-4.7 and gcc-4.8

--- src/external/downloadPyPlusPlus.cmake
+++ src/external/downloadPyPlusPlus.cmake
@@ -1,9 +1,9 @@
 # cmake's file(DOWNLOAD ...) command doesn't do https until version 2.8.10
 file(DOWNLOAD
-    https://github.com/gccxml/gccxml/archive/2cbeb9d631e0198fcbeca3d230ef49fe07e87dd8.tar.gz
-    "${TEMPDIR}/gccxml-2012-11-06.tgz"
+    https://github.com/gccxml/gccxml/archive/567213ac765c99d5dfd23b14000b3c7b76274fcb.tar.gz
+    "${TEMPDIR}/gccxml-2013-05-02.tgz"
     SHOW_PROGRESS
-    EXPECTED_MD5 e2ad8bd675af9009530e5e175ed441bc)
+    EXPECTED_MD5 30f30e7cb246b8f00e58fa8562ce1c4c)
 file(DOWNLOAD
     https://bitbucket.org/ompl/pygccxml/downloads/pygccxml-r575.tgz
     "${TEMPDIR}/pygccxml-r575.tgz"
