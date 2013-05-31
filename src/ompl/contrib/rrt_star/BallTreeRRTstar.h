bbbfbe23aa50f10c29abedd2d03783fce9de61c1
Ioan Sucan <isucan@willowgarage.com>
2013-05-31 14:33:03 +0300

    add headers that forward includes for RRTstar (for backward compatibility in the next release)

---
+++ src/ompl/contrib/rrt_star/BallTreeRRTstar.h
@@ -0,0 +1,2 @@
+#warning "Please include <ompl/geometric/planners/rrt/BallTreeRRTstar.h> directly"
+#include "ompl/geometric/planners/rrt/BallTreeRRTstar.h"
---
+++ src/ompl/contrib/rrt_star/RRTstar.h
@@ -0,0 +1,2 @@
+#warning "Please include <ompl/geometric/planners/rrt/RRTstar.h> directly"
+#include "ompl/geometric/planners/rrt/RRTstar.h"
