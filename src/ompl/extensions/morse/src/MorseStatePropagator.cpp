5cb67c0933656786d152469daa6b97ba295ae1d4
Caleb Voss <voss.caleb@gmail.com>
2013-09-23 10:48:14 -0500

    Use boost::mutex::scoped_lock to clean up code.

--- src/ompl/extensions/morse/src/MorseStatePropagator.cpp
+++ src/ompl/extensions/morse/src/MorseStatePropagator.cpp
@@ -49,34 +49,25 @@ ompl::control::MorseStatePropagator::MorseStatePropagator(const SpaceInformation
 
 void ompl::control::MorseStatePropagator::propagate(const base::State *state, const Control* control, const double duration, base::State *result) const
 {
-    env_->mutex_.lock();
+    boost::mutex::scoped_lock lock(env_->mutex_);
 
-    try
-    {
-        // place the MORSE world at the start state
-        si_->getStateSpace()->as<base::MorseStateSpace>()->writeState(state);
+    // place the MORSE world at the start state
+    si_->getStateSpace()->as<base::MorseStateSpace>()->writeState(state);
 
-        // convert control into vector of doubles
-        std::vector<double> controlVec;
-        const double *conVals = control->as<RealVectorControlSpace::ControlType>()->values;
-        for (unsigned int i = 0; i < env_->controlDim_; i++)
-            controlVec.push_back(conVals[i]);
+    // convert control into vector of doubles
+    std::vector<double> controlVec;
+    const double *conVals = control->as<RealVectorControlSpace::ControlType>()->values;
+    for (unsigned int i = 0; i < env_->controlDim_; i++)
+        controlVec.push_back(conVals[i]);
 
-        // apply the controls
-        env_->applyControl(controlVec);
+    // apply the controls
+    env_->applyControl(controlVec);
 
-        // propagate one step forward
-        env_->worldStep(duration);
+    // propagate one step forward
+    env_->worldStep(duration);
 
-        // read the final state from the MORSE world
-        si_->getStateSpace()->as<base::MorseStateSpace>()->readState(result);
-    }
-    catch (...)
-    {
-        env_->mutex_.unlock();
-        throw;
-    }
-    env_->mutex_.unlock();
+    // read the final state from the MORSE world
+    si_->getStateSpace()->as<base::MorseStateSpace>()->readState(result);
 }
 
 bool ompl::control::MorseStatePropagator::canPropagateBackward(void) const
