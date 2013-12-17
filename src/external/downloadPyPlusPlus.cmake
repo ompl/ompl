de0d2e3578758c4ac770735914b8946be379bb67
Mark Moll <mmoll@rice.edu>
2013-12-17 13:41:49 -0600

    use latest gccxml snapshot (needed for gcc48)

--- src/external/downloadPyPlusPlus.cmake
+++ src/external/downloadPyPlusPlus.cmake
@@ -1,7 +1,7 @@
 # cmake's file(DOWNLOAD ...) command doesn't do https until version 2.8.10
 file(DOWNLOAD
-    https://github.com/gccxml/gccxml/archive/567213ac765c99d5dfd23b14000b3c7b76274fcb.tar.gz
-    "${TEMPDIR}/gccxml-2013-05-02.tgz" SHOW_PROGRESS)
+    https://github.com/gccxml/gccxml/archive/b040a46352e4d5c11a0304e4fcb6f7842008942a.tar.gz
+    "${TEMPDIR}/gccxml-2013-09-19.tgz" SHOW_PROGRESS)
 file(DOWNLOAD
     https://bitbucket.org/ompl/pygccxml/downloads/pygccxml-r579.tgz
     "${TEMPDIR}/pygccxml-r579.tgz" SHOW_PROGRESS)
