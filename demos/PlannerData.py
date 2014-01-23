91dce0e37e5330e2972fe98038086ac7fd4d5066
Mark Moll <mmoll@rice.edu>
2014-01-23 10:49:13 -0600

    clean up

--- demos/PlannerData.py
+++ demos/PlannerData.py
@@ -57,7 +57,7 @@ try:
 except:
     # if the ompl module is not in the PYTHONPATH assume it is installed in a
     # subdirectory of the parent directory called "py-bindings."
-    from os.path import basename, abspath, dirname, join
+    from os.path import abspath, dirname, join
     import sys
     sys.path.insert(0, join(dirname(dirname(abspath(__file__))),'py-bindings'))
     from ompl import util as ou
