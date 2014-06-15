9b7b2152f84919fb952fa109261d2321b26b6f0c
Dave Coleman <davetcoleman@gmail.com>
2014-06-15 17:02:00 +0900

    Added a boost Ptr version of PathGeometric

--- src/ompl/geometric/PathGeometric.h
+++ src/ompl/geometric/PathGeometric.h
@@ -49,6 +49,14 @@ namespace ompl
     namespace geometric
     {
 
+        /// @cond IGNORE
+        /** \brief Forward declaration of ompl::base::SpaceInformation */
+        OMPL_CLASS_FORWARD(PathGeometric);
+        /// @endcond
+
+        /** \class ompl::base::PathGeometricPtr
+            \brief A boost shared pointer wrapper for ompl::base::PathGeometric */
+
         /** \brief Definition of a geometric path.
 
             This is the type of path computed by geometric planners. */
@@ -145,7 +153,8 @@ namespace ompl
             /** \brief Check if the path is valid. If it is not,
                 attempts are made to fix the path by sampling around
                 invalid states. Not more than \e attempts samples are
-                drawn. A pair of boolean values is returned. The first
+                drawn. 
+                \return A pair of boolean values is returned. The first
                 value represents the validity of the path before any
                 change was made. The second value represents the
                 validity of the path after changes were attempted. If
