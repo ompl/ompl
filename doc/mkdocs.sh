f9d774dbf6d1639e967d4e8d8553922aad581b21
Mark Moll <mmoll@rice.edu>
2014-07-08 10:29:25 -0500

    small doc fixes

--- doc/mkdocs.sh
+++ doc/mkdocs.sh
@@ -2,9 +2,5 @@
 rm -rf html
 doxygen Doxyfile
 cd html
-# for doxygen >= 1.8.4
 for f in md_doc_markdown_*; do mv $f `echo $f | cut -c17-1000`; done
-# for doxygen >= 1.8.0 and < 1.8.4
-# for f in md_*; do mv $f `echo $f | cut -c4-1000`; done
-# mv mainpage.html index.html
 rm *8md_source.html
