13dc0b09818d0301db5eb641a8deebde2fe73741
Mark Moll <mmoll@rice.edu>
2015-02-27 08:26:00 -0600

    doc fix

--- doc/mkdocs.sh
+++ doc/mkdocs.sh
@@ -7,7 +7,7 @@ cd html
 for f in md_doc_markdown_*; do mv $f `echo $f | cut -c17-1000`; done
 rm *8md_source.html
 
-for f in *.html; do
-    sed -i "" 's/href="md_ompl_doc_markdown_/href="/g;s/href="md_doc_markdown_/href="/g' $f
+for f in *.html search/*.js; do
+    sed -i "" 's/href="md_doc_markdown_/href="/g' $f
 done
 
