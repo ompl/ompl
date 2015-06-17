dff67222be5ad1f3b565d8acdce06de9cf70edb5
Dave Coleman <davetcoleman@gmail.com>
2015-06-16 23:20:02 -0700

    Moved ANSI colors to header files

--- src/ompl/util/src/Console.cpp
+++ src/ompl/util/src/Console.cpp
@@ -39,22 +39,6 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdarg>
-#ifdef _WIN32
-#include <stdio.h>
-#include <io.h>
-#define isatty(x) _isatty(x)
-#define fileno(x) _fileno(x)
-#else
-#include <unistd.h>
-#endif
-
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
 
 /// @cond IGNORE
 
