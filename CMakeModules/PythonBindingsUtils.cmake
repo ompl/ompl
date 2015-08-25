3048a2f46789452035596156540469bd16756806
Mark Moll <mmoll@rice.edu>
2015-08-25 15:26:50 -0500

    add comment for last commit

--- CMakeModules/PythonBindingsUtils.cmake
+++ CMakeModules/PythonBindingsUtils.cmake
@@ -20,6 +20,8 @@ if(APPLE)
     set(PYOMPL_EXTRA_CFLAGS "-m64")
 endif(APPLE)
 
+# Trick gccxml to ignore some compiler intrinsics that are used in Boost.Atomic
+# in Boost 1.55.
 if(CMAKE_COMPILER_IS_GNUCXX AND BOOST_VERSION VERSION_GREATER "1.54.0")
     set(PYOMPL_EXTRA_CFLAGS "${PYOMPL_EXTRA_CFLAGS} -DBOOST_INTEL_CXX_VERSION")
 endif()
