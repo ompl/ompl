8ef28c13c06ffb5d6ad6995680fd4d025abbc0b4
Mark Moll <mmoll@rice.edu>
2015-11-01 10:54:15 -0600

    build fix for cases where CMAKE_MODULE_PATH is set externally

--- CMakeModules/FindEigen3.cmake
+++
@@ -1,8 +0,0 @@
-include(FindPackageHandleStandardArgs)
-
-find_package(PkgConfig)
-if(PKG_CONFIG_FOUND)
-    pkg_check_modules(EIGEN3 eigen3)
-endif()
-
-find_package_handle_standard_args(Eigen3 DEFAULT_MSG EIGEN3_INCLUDE_DIRS)
