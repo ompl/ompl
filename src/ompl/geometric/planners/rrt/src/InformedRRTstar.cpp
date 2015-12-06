b32d72d9e3e08376c415c6b190de8f19a1e1eb34
Jonathan Gammell <jon.gammell@utoronto.ca>
2015-12-06 14:30:53 -0500

    Made Informed RRT* config a little more explicit.

--- src/ompl/geometric/planners/rrt/src/InformedRRTstar.cpp
+++ src/ompl/geometric/planners/rrt/src/InformedRRTstar.cpp
@@ -43,11 +43,17 @@ ompl::geometric::InformedRRTstar::InformedRRTstar(const base::SpaceInformationPt
     setName("InformedRRTstar");
 
     //Configure RRTstar to be InformedRRT*:
+    setAdmissibleCostToCome(true);
     setInformedSampling(true);
     setTreePruning(true);
     setPrunedMeasure(true);
 
+    //Disable conflicting options
+    setSampleRejection(false);
+    setNewStateRejection(false);
+
     //Remove those parameters:
+    params_.remove("use_admissible_heuristic");
     params_.remove("informed_sampling");
     params_.remove("pruned_measure");
     params_.remove("tree_pruning");
