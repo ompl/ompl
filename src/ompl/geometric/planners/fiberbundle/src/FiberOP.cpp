b637179ce5af4c69b9eda80c7b625395b9718b50
Andreas Orthey <andreas.orthey@gmx.de>
2019-11-29 22:10:43 +0100

    Add return values for qrrt during optimization

--- src/ompl/geometric/planners/fiberbundle/src/FiberOP.cpp
+++ src/ompl/geometric/planners/fiberbundle/src/FiberOP.cpp
@@ -1,5 +1,6 @@
 #include <ompl/geometric/planners/fiberbundle/FiberOP.h>
 #include <ompl/geometric/planners/quotientspace/QRRT.h>
+#include <ompl/util/Time.h>
 
 namespace og = ompl::geometric;
 namespace ob = ompl::base;
@@ -37,12 +38,30 @@ ompl::base::PlannerStatus ompl::geometric::FiberOP::solve(const base::PlannerTer
       std::static_pointer_cast<og::QRRT>(planner)->setStopLevel(1);
       unfilteredPlanners_.push_back(planner);
   }
+
+
+
   std::cout << "Iteration " << iteration_ << std::endl;
   double timeToPlan = 0.5;
   for(uint k = 0; k < unfilteredPlanners_.size(); k++){
     ob::PlannerPtr planner = unfilteredPlanners_.at(k);
     ob::PlannerTerminationCondition ptc( ob::timedPlannerTerminationCondition(timeToPlan) );
+    time::point timeStart = time::now();
     planner->solve(ptc);
+    double timeUsed_ = time::seconds(time::now() - timeStart);
+
+    std::cout << "Time spend on first QS level: " << timeUsed_ << std::endl;
+    /** \brief Retrieve a planner's planner progress property map */
+    //using PlannerProgressProperty = std::function<std::string()>;
+    //using PlannerProgressProperties = std::map<std::string, PlannerProgressProperty>;
+    PlannerProgressProperties prop =  planner->getPlannerProgressProperties();
+    std::cout << prop.size() << std::endl;
+    for(auto x : prop) 
+    {
+      std::cout << x.first << std::endl;
+
+    }
+
 
   }
 }
