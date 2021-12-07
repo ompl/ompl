574a0651813b76ca5dcfa3e36d2b4b356956f862
Wolfgang Hoenig <whoenig@caltech.edu>
2021-12-07 17:17:58 +0100

    fix cost computation
    Optimization Objectives: Add support for control-space planners
    
    OptimizationObjective was designed for geometric planning and cannot
    associate costs with ompl::control::Control. This addresses this short-
    coming in the following way:
    
    * New controlCost(control, steps) function (defaults to identityCost)
    * New ControlDurationObjective, which minimizes the total time needed
    * Support for controlCost() in the control-space version of SST
    
    Other typical control-space costs include control-integral costs, but
    are application-specific and can be now defined by the user by creating
    a derived class inheriting from OptimizationObjective().

--- src/ompl/control/planners/sst/src/SST.cpp
+++ src/ompl/control/planners/sst/src/SST.cpp
@@ -266,7 +266,9 @@ ompl::base::PlannerStatus ompl::control::SST::solve(const base::PlannerTerminati
 
         if (propCd == cd)
         {
-            base::Cost incCost = opt_->motionCost(nmotion->state_, rstate);
+            base::Cost incCostMotion = opt_->motionCost(nmotion->state_, rstate);
+            base::Cost incCostControl = opt_->controlCost(rctrl, cd);
+            base::Cost incCost = opt_->combineCosts(incCostMotion, incCostControl);
             base::Cost cost = opt_->combineCosts(nmotion->accCost_, incCost);
             Witness *closestWitness = findClosestWitness(rmotion);
 
