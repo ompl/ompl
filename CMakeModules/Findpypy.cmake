b82f6953a1698016f28f572d175192347f4afe6f
Zachary Kingston <zak@rice.edu>
2023-05-01 10:09:11 -0500

    Support for Python binding installation through pip & pypy version checking (#1022)
    * setup.py for installing bindings through pip
    
    * update gitignore with python files
    
    * Feat: check pypy version against python version to prevent binding generation errors
    
    * include_package_data for setup.py
    
    * removed unnecessary include_package_data
    
    * Update CMakeModules/Findpypy.cmake
    
    Co-authored-by: Wil Thomason <wbthomason@users.noreply.github.com>
    
    * Updated pypy version check to exclude pypy2 + added status message
    
    ---------
    
    Co-authored-by: Wil Thomason <wil.thomason@gmail.com>
    Co-authored-by: Wil Thomason <wbthomason@users.noreply.github.com>

--- CMakeModules/Findpypy.cmake
+++ CMakeModules/Findpypy.cmake
@@ -5,8 +5,18 @@ include(FindPackageHandleStandardArgs)
 find_program(PYPY NAMES pypy${PYTHON_VERSION_MAJOR} pypy)
 
 if(PYPY)
+  execute_process(
+    COMMAND ${PYPY} "-c" "import platform; print(platform.python_version())"
+    OUTPUT_VARIABLE PYPY_PYTHON_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if(${PYPY_PYTHON_VERSION} VERSION_GREATER_EQUAL ${PYTHON_VERSION_MAJOR})
     set(PYTHON_BINDING_EXEC "${PYPY}")
-else()
+  else()
     set(PYTHON_BINDING_EXEC "${PYTHON_EXEC}")
+    message(STATUS "Could NOT use pypy ${PYPY_PYTHON_VERSION} - incompatable with Python version ${PYTHON_VERSION_FULL}")
+  endif()
+else()
+  set(PYTHON_BINDING_EXEC "${PYTHON_EXEC}")
 endif()
+
 find_package_handle_standard_args(pypy DEFAULT_MSG PYPY)
---
+++ py-bindings/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ompl',
+    version='1.6.0',
+    description='The Open Motion Planning Library',
+    author='Ioan A. Șucan, Mark Moll, Zachary Kingston, Lydia E. Kavraki',
+    author_email='zak@rice.edu',
+    url='https://ompl.kavrakilab.org',
+    packages=['ompl'],
+)
