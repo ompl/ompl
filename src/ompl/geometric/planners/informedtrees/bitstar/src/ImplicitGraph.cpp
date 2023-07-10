055f88e12132d9246f90cb9d1ceb732bed67f523
Marlin Strub <marlin.strub@gmail.com>
2023-07-09 18:18:30 -0700

    Only prune goal if it is not part of the current best solution

--- src/ompl/geometric/planners/informedtrees/bitstar/src/ImplicitGraph.cpp
+++ src/ompl/geometric/planners/informedtrees/bitstar/src/ImplicitGraph.cpp
@@ -1093,8 +1093,14 @@ namespace ompl
                 // Run until at the end:
                 while (goalIter != goalEnd)
                 {
-                    // Check if this goal has met the criteria to be pruned
-                    if (this->canSampleBePruned(*goalIter))
+                    // Check if this goal has met the criteria to be pruned, but make sure it is not the goal that is
+                    // the current best solution. The current goal can satisfy the condition for pruning if the
+                    // heuristic is perfect, since samples are pruned unless the combined costs of the heuristic
+                    // cost-to-come and heuristic cost-to-go is (strictly) lower than the current solution cost. If the
+                    // heuristic is perfect, then the heuristic cost-to-come is equal to the solution cost (and the
+                    // heuristic cost-to-go is zero, since this is a goal).
+                    if (this->canSampleBePruned(*goalIter) &&
+                        costHelpPtr_->isCostNotEquivalentTo((*goalIter)->getCost(), solutionCost_))
                     {
                         // It has, remove the goal vertex completely
                         // Check if this vertex is in the tree
