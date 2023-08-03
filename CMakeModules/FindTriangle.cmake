621d0da548bdf2cf8e485f799d4fa9cf967da525
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-08-02 21:43:30 -0600

    Add support for triangle imported library
    * The generators work better this way
    * Copied from https://cmake.org/cmake/help/latest/manual/cmake-developer.7.html
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- CMakeModules/FindTriangle.cmake
+++ CMakeModules/FindTriangle.cmake
@@ -10,3 +10,11 @@ find_path(TRIANGLE_INCLUDE_DIR triangle.h PATH_SUFFIXES triangle
     DOC "Location of Triangle header file directory")
 find_package_handle_standard_args(Triangle DEFAULT_MSG TRIANGLE_LIBRARY TRIANGLE_INCLUDE_DIR)
 mark_as_advanced(TRIANGLE_LIBRARY TRIANGLE_INCLUDE_DIR)
+
+if(Triangle_FOUND AND NOT TARGET Triangle::Triangle)
+    add_library(Triangle::Triangle UNKNOWN IMPORTED)
+    set_target_properties(Triangle::Triangle PROPERTIES
+        IMPORTED_LOCATION "${TRIANGLE_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${TRIANGLE_INCLUDE_DIR}"
+    )
+endif()
