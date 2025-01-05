d7715184c23c2b2aef4b71d22637c42ba7fb74ba
JafarAbdi <jafar.uruc@gmail.com>
2025-01-05 01:29:37 +0000

    Update boost version to support numpy>=2.0

--- .github/workflows/before_build.sh
+++ .github/workflows/before_build.sh
@@ -4,7 +4,7 @@ set -eux
 
 # Dependency versions.
 castxml_version="0.6.8" # version specifier for Linux only
-boost_version="1.86.0"
+boost_version="1.87.0"
 
 # Collect some information about the build target.
 build_os="$(uname)"
@@ -20,10 +20,7 @@ install_boost() {
     # multiple on the host system.
     python_include_path=$(python3 -c "from sysconfig import get_paths as gp; print(gp()['include'])")
     echo "using python : ${python_version} : : ${python_include_path} ;" > "$HOME/user-config.jam"
-
-    # TODO: As of boost-1.86.0, numpy>=2.0 is not supported.
-    # See: https://github.com/boostorg/python/issues/431
-    pip3 install "numpy<2.0"
+    pip3 install numpy
 
     ./bootstrap.sh
     sudo ./b2 "${b2_args[@]}" \
