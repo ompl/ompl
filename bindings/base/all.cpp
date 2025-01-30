18b05f5abf2319dd918b0ca114919e6c2aeb866d
Weihang Guo <weihang.guo2001@gmail.com>
2025-01-30 17:51:59 -0600

    Build example, not finish

--- bindings/base/all.cpp
+++ bindings/base/all.cpp
@@ -4,11 +4,15 @@ namespace nb = nanobind;
 
 // Forward declarations
 void initState(nb::module_&);
+void initStateSpace(nb::module_&);
 void initSE2StateSpace(nb::module_&);
 void initRealVectorStateSpace(nb::module_&);
+void initRealVectorBounds(nb::module_&);
 
 NB_MODULE(base, m) {
     initState(m);
+    initStateSpace(m);
     initSE2StateSpace(m);
     initRealVectorStateSpace(m);
+    initRealVectorBounds(m);
 }
