bb65cfd718126c55a41e339d9ffb5a1baffdbce2
Weihang <weihang.guo2001@gmail.com>
2025-04-10 23:28:41 -0500

    Backup

--- bindings/control/ControlSampler.cpp
+++ bindings/control/ControlSampler.cpp
@@ -4,6 +4,9 @@
 #include <nanobind/stl/string.h>
 
 #include "ompl/control/ControlSampler.h"
+#include "ompl/control/Control.h"
+#include "ompl/control/ControlSpace.h"
+
 #include "init.hh"
 
 namespace nb = nanobind;
@@ -13,33 +16,29 @@ namespace ob = ompl::base;
 
 void ompl::binding::control::init_ControlSampler(nb::module_& m)
 {
-    nb::class_<ompl::control::ControlSampler>(m, "ControlSampler");
+    // TODO: Virtual class
+    nb::class_<oc::ControlSampler>(m, "ControlSampler");
 
+    // TODO: Virtual method addSampler
     nb::class_<oc::CompoundControlSampler, oc::ControlSampler>(m, "CompoundControlSampler")
         .def(nb::init<const oc::ControlSpace*>(),
              nb::arg("space"),
              "Construct a CompoundControlSampler for the given control space")
-        
         // Overloaded sample() methods:
         .def("sample", 
-             nb::overload_cast<oc::Control *>(&oc::CompoundControlSampler::sample, nb::const_),
+             nb::overload_cast<oc::Control *>(&oc::CompoundControlSampler::sample),
              nb::arg("control"),
              "Sample a control uniformly")
-        
         .def("sample", 
-             nb::overload_cast<oc::Control *, const ob::State *>(&oc::CompoundControlSampler::sample, nb::const_),
+             nb::overload_cast<oc::Control *, const ob::State *>(&oc::CompoundControlSampler::sample),
              nb::arg("control"), nb::arg("state"),
              "Sample a control uniformly near the given state")
-        
-        // Overloaded sampleNext() methods:
         .def("sampleNext",
-             nb::overload_cast<oc::Control *, const oc::Control *>(&oc::CompoundControlSampler::sampleNext, nb::const_),
+             nb::overload_cast<oc::Control *, const oc::Control *>(&oc::CompoundControlSampler::sampleNext),
              nb::arg("control"), nb::arg("previous"),
              "Sample the next control based on the previous control")
-        
         .def("sampleNext",
-             nb::overload_cast<oc::Control *, const oc::Control *, const ob::State *>(&oc::CompoundControlSampler::sampleNext, nb::const_),
+             nb::overload_cast<oc::Control *, const oc::Control *, const ob::State *>(&oc::CompoundControlSampler::sampleNext),
              nb::arg("control"), nb::arg("previous"), nb::arg("state"),
-             "Sample the next control based on the previous control and a given state")
-        ;
+             "Sample the next control based on the previous control and a given state");
 }
---
+++ pytests/ControlStateSpaces.py
@@ -0,0 +1,5 @@
+import sys
+import pytest
+
+from ompl import base as ob
+from ompl import control as oc
