379bd321d21aaac7eb5e9f3755e39d612b227d68
Weihang Guo <weihang.guo2001@gmail.com>
2025-12-13 10:40:39 -0600

    Revert "Add CPM"
    This reverts commit b40449a142b7efb0ff62d645e9b2a1000336ab9a.

--- CMakeModules/FetchInitCPM.cmake
+++
@@ -1,8 +0,0 @@
-file(
-  DOWNLOAD
-  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.1/CPM.cmake
-  ${CMAKE_CURRENT_BINARY_DIR}/CMakeModules/CPM.cmake
-  EXPECTED_HASH SHA256=117cbf2711572f113bab262933eb5187b08cfc06dce0714a1ee94f2183ddc3ec
-)
-set(CPM_USE_LOCAL_PACKAGES ON)
-include(${CMAKE_CURRENT_BINARY_DIR}/CMakeModules/CPM.cmake)
