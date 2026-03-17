ddffecf6381098f7b317e4ca6b50991c14046896
EmilianoHFlores <ef55@rice.edu>
2026-03-17 16:27:04 -0500

    Refactored VampCagePlanningBenchmark, now uses classes from vamp state space and includes visualization

--- demos/vamp/VampPlanning.py
+++ demos/vamp/VampPlanning.py
@@ -7,6 +7,12 @@ from fire import Fire
 from ompl import base as ob
 from ompl import geometric as og
 from ompl import tools as ot
+from vamp_state_space import VampMotionValidator, VampStateValidityChecker, VampStateSpace
+
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+from viser_visualizer.viser_visualizer import ViserVisualizer
 
 import time
 import subprocess
@@ -36,16 +42,6 @@ problem = [
     [0.35, -0.35, 0.8],
     ]
 
-class VampMotionValidator(ob.MotionValidator):
-    def __init__(self, si, env: vamp.Environment, dimension: int):
-        super().__init__(si)
-        self.env = env
-        self.dimension = dimension
-
-    def checkMotion(self, s1:ob.RealVectorStateType, s2:ob.RealVectorStateType) -> bool:
-        config1 = s1[0:self.dimension]
-        config2 = s2[0:self.dimension]
-        return vamp.panda.validate_motion(config1, config2, self.env)
 
 def isStateValid(s:ob.RealVectorStateType, env:vamp.Environment, dimension: int) -> bool:
     config = s[0:dimension]
@@ -53,84 +49,113 @@ def isStateValid(s:ob.RealVectorStateType, env:vamp.Environment, dimension: int)
 
 def planOnce(
     variation: float = 0.01, 
-    radius: float = 0.2
+    radius: float = 0.2,
+    visualize: bool = False
     ):
-    robot = vamp.panda
-    dimension = robot.dimension()
-    space = ob.RealVectorStateSpace(dimension)
-    bounds = ob.RealVectorBounds(dimension)
-    upper_bounds = robot.upper_bounds()
-    lower_bounds = robot.lower_bounds()
-
-    for i in range(dimension):
-        bounds.setLow(i, lower_bounds[i])
-        bounds.setHigh(i, upper_bounds[i])
-    space.setBounds(bounds)
-
+    
+    # Create VAMP spherized environment
     spheres = [np.array(sphere) for sphere in problem]
     random.shuffle(spheres)
     spheres_copy = copy.deepcopy(spheres)
 
-    e = vamp.Environment()
+    env = vamp.Environment()
     for sphere in spheres_copy:
         sphere += np.random.uniform(low = -variation, high = variation, size = (3, ))
-        e.add_sphere(vamp.Sphere(sphere, radius))
-    planning_start = time.time()
+        env.add_sphere(vamp.Sphere(sphere, radius))
+    
+    # Use VAMP's robot module to initialize state space and validations
+    robot = vamp.panda
+    dimension = robot.dimension()
+    space = VampStateSpace(robot=robot)
     si = ob.SpaceInformation(space)
+    
+    motion_validator = VampMotionValidator(si = si, env = env, robot = robot)
+    state_validity_checker = VampStateValidityChecker(si = si, env = env, robot = robot)
+    
+    # Select a planner (optional, OMPL will choose one if not provided
     planner = og.RRTConnect(si)
-    si.setMotionValidator(VampMotionValidator(si, e, dimension))
-    si.setStateValidityChecker(partial(isStateValid, env=e, dimension=dimension))
+    
+    # Set validators
+    si.setMotionValidator(motion_validator)
+    si.setStateValidityChecker(state_validity_checker)
+    
+    # Build SimpleSetup object
     ss = og.SimpleSetup(si)
     ss.setPlanner(planner)
+    
+    # create start and goal states
     start = si.allocState()
     start[0:dimension] = a
     goal = si.allocState()
     goal[0:dimension] = b
     ss.setStartAndGoalStates(start, goal)
-    result = ss.solve(1.0)
+    
+    # solve
+    planning_start = time.time()
+    result = ss.solve(5.0)
     if result: 
         path = ss.getSolutionPath()
-        print(path)
+        print(f"Planning time: {time.time() - planning_start}")
+        print(f"Generated path with {path.getStateCount()} states")
+        
+        if visualize:
+            visualizer = ViserVisualizer(robot_name="panda", 
+                                        port=8080)
+            for sphere in spheres:
+                visualizer.add_sphere(sphere, radius)
+            
+            path.interpolate(50)
+            states = path.getStates()
+            trajectory = np.array([list(state[0:dimension]) for state in states])
+            visualizer.visualize_trajectory(trajectory)
+            visualizer.play_until_key_pressed()
+            
 
-def planBenchmark(variation: float = 0.01, radius: float = 0.2, n_trials: int = 100):
-    robot = vamp.panda
-    dimension = robot.dimension()
-    space = ob.RealVectorStateSpace(dimension)
-    bounds = ob.RealVectorBounds(dimension)
-    upper_bounds = robot.upper_bounds()
-    lower_bounds = robot.lower_bounds()
 
-    for i in range(dimension):
-        bounds.setLow(i, lower_bounds[i])
-        bounds.setHigh(i, upper_bounds[i])
-    space.setBounds(bounds)
-
+def planBenchmark(variation: float = 0.01, radius: float = 0.2, n_trials: int = 100, visualize: bool = False):
+    
+    if visualize:
+        print("Benchmark results are not visualized")
+    # Create VAMP spherized environment
     spheres = [np.array(sphere) for sphere in problem]
     random.shuffle(spheres)
     spheres_copy = copy.deepcopy(spheres)
 
-    e = vamp.Environment()
+    env = vamp.Environment()
     for sphere in spheres_copy:
         sphere += np.random.uniform(low = -variation, high = variation, size = (3, ))
-        e.add_sphere(vamp.Sphere(sphere, radius))
-    planning_start = time.time()
+        env.add_sphere(vamp.Sphere(sphere, radius))
+    
+    # Use VAMP's robot module to initialize state space and validations
+    robot = vamp.panda
+    dimension = robot.dimension()
+    space = VampStateSpace(robot=robot)
     si = ob.SpaceInformation(space)
-    planner = og.RRTConnect(si)
-    si.setMotionValidator(VampMotionValidator(si, e, dimension))
-    si.setStateValidityChecker(partial(isStateValid, env=e, dimension=dimension))
+    
+    motion_validator = VampMotionValidator(si = si, env = env, robot = robot)
+    state_validity_checker = VampStateValidityChecker(si = si, env = env, robot = robot)
+    
+    # Set validators
+    si.setMotionValidator(motion_validator)
+    si.setStateValidityChecker(state_validity_checker)
+    
+    # Build SimpleSetup object
     ss = og.SimpleSetup(si)
-    ss.setPlanner(planner)
+    
     start = si.allocState()
     start[0:dimension] = a
     goal = si.allocState()
     goal[0:dimension] = b
     ss.setStartAndGoalStates(start, goal)
+    
+    # Create Benchmark
     benchmark = ot.Benchmark(ss, "Vamp Cage Planning Benchmark")
     benchmark.addPlanner(og.RRTConnect(si))
     benchmark.addPlanner(og.RRT(si))
     benchmark.addPlanner(og.KPIECE1(si))
     benchmark.addPlanner(og.LBKPIECE1(si))
 
+    # Setup Benchmark
     req = ot.Request()
     req.maxTime = 1.0
     req.maxMem = 100.0
@@ -148,6 +173,7 @@ def planBenchmark(variation: float = 0.01, radius: float = 0.2, n_trials: int = 
     try:
         import plannerarena.app
         # Override the database path directly in the module
+        print(f"Displaying benchmark results from {db_path}")
         plannerarena.app.DATABASE = os.path.abspath(db_path)
         plannerarena.app.run()
     except ImportError:
@@ -155,15 +181,16 @@ def planBenchmark(variation: float = 0.01, radius: float = 0.2, n_trials: int = 
 
 def main(
     variation: float = 0.01,
-    benchmark: bool = True,
+    benchmark: bool = False,
     n_trials: int = 100,
     radius: float = 0.2,
+    visualize: bool = False
     ):
 
     if benchmark:
-        planBenchmark(variation, radius, n_trials)
+        planBenchmark(variation, radius, n_trials, visualize)
     else: 
-        planOnce(variation, radius)
+        planOnce(variation, radius, visualize)
    
         
 if __name__ == "__main__":
