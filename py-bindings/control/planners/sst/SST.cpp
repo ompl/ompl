b34b09792047a0f406b305d127826e96b877beab
Zhuoyun Zhong <zhuoyun.zhong@siemens.com>
2026-08-14 11:34:56 -0700

    add SST bindings

--- py-bindings/control/planners/sst/SST.cpp
+++ py-bindings/control/planners/sst/SST.cpp
@@ -1,4 +1,9 @@
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/shared_ptr.h>
+
+#include "ompl/base/PlannerData.h"
+#include "ompl/base/PlannerTerminationCondition.h"
+#include "ompl/control/SpaceInformation.h"
 #include "ompl/control/planners/sst/SST.h"
 #include "../../init.h"
 
@@ -8,5 +13,17 @@ namespace ob = ompl::base;
 
 void ompl::binding::control::initPlannersSst_SST(nb::module_ &m)
 {
-    nb::class_<oc::SST, ob::Planner>(m, "SST");
+    nb::class_<oc::SST, ob::Planner>(m, "SST")
+        .def(nb::init<const oc::SpaceInformationPtr &>(), nb::arg("si"))
+        .def("setup", &oc::SST::setup)
+        .def("solve", &oc::SST::solve, nb::arg("terminationCondition"))
+        .def("getPlannerData", [](const oc::SST &planner, ob::PlannerData &data) { planner.getPlannerData(data); },
+             nb::arg("data"))
+        .def("clear", &oc::SST::clear)
+        .def("setGoalBias", &oc::SST::setGoalBias, nb::arg("goalBias"))
+        .def("getGoalBias", &oc::SST::getGoalBias)
+        .def("setSelectionRadius", &oc::SST::setSelectionRadius, nb::arg("selectionRadius"))
+        .def("getSelectionRadius", &oc::SST::getSelectionRadius)
+        .def("setPruningRadius", &oc::SST::setPruningRadius, nb::arg("pruningRadius"))
+        .def("getPruningRadius", &oc::SST::getPruningRadius);
 }
